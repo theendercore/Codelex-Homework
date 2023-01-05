096443ebd38d54269a95594f92ed46d9e6656bcc
theendercore <theendercore@gmail.com>
2023-01-06 01:04:56 +0200

    1 uzd

--- homework-4/src/scripts.ts
+++ homework-4/src/scripts.ts
@@ -1,6 +1,11 @@
-console.log('Ready for coding');
-
-
-function hi(){
-    
+function log(args: any) {
+  console.log(args);
 }
+
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+log(sum(1,3))
+log(sum(1,10))
+log(sum(99,1))
+console.log("Ready for coding");
