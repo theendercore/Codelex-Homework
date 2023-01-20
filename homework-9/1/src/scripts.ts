8276aaf907e5d1511a2c6817deec03d8b831bb48
theendercore <theendercore@gmail.com>
2023-01-20 15:24:43 +0200

    added the load more button

--- homework-9/1/src/scripts.ts
+++ homework-9/1/src/scripts.ts
@@ -1,10 +1,15 @@
 import { newCard } from "./assets/ts/helper";
 import { Character, Episode, request } from "./assets/ts/interfaces";
 
-const box = document.querySelector<HTMLDivElement | null>(".container");
+const box = document.querySelector<HTMLDivElement | null>(".box");
+const nextBtn = document.querySelector<HTMLButtonElement | null>(".next");
+
+let nextRequest: string;
+
 fetch("https://rickandmortyapi.com/api/character")
   .then((response) => response.json())
-  .then((data: request) =>
+  .then((data: request) => {
+    nextRequest = data.info.next;
     data.results.forEach((c: Character) => {
       fetch(c.episode[0])
         .then((response) => response.json())
@@ -20,5 +25,33 @@ fetch("https://rickandmortyapi.com/api/character")
               ep.name
             ))
         );
-    })
-  );
+    });
+  });
+
+nextBtn.addEventListener("click", () => {
+  if (nextRequest) {
+    fetch(nextRequest)
+      .then((response) => response.json())
+      .then((data: request) => {
+        nextRequest = data.info.next;
+        data.results.forEach((c: Character) => {
+          fetch(c.episode[0])
+            .then((response) => response.json())
+            .then(
+              (ep: Episode) =>
+                (box.innerHTML += newCard(
+                  c.image,
+                  c.name,
+                  c.status,
+                  c.species,
+                  c.origin.name,
+                  c.location.name,
+                  ep.name
+                ))
+            );
+        });
+      });
+  } else {
+    alert("Loading! Please wait...");
+  }
+});
