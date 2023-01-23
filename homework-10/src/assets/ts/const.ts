2cbbcf89893b7cd881209f1109c4dc084e86e2ca
theendercore <theendercore@gmail.com>
2023-01-23 18:24:49 +0200

    finished

--- homework-10/src/assets/ts/const.ts
+++ homework-10/src/assets/ts/const.ts
@@ -1,6 +1,9 @@
 const box = document.querySelector<HTMLDivElement>(".box");
 const dbUrl = "http://localhost:3004/players";
+const addScreen = document.querySelector<HTMLDivElement>(".add-screen");
+const addTitle = addScreen.querySelector<HTMLInputElement>(".add-screen--title");
+const addText = addScreen.querySelector<HTMLInputElement>(".add-screen--text");
+const addURL = document.querySelector<HTMLInputElement>(".add-screen--skin");
+const addBtn = addScreen.querySelector<HTMLButtonElement>(".add-screen__btn");
 
-
-
-export { box, dbUrl };
+export { box, dbUrl, addText, addTitle, addURL, addBtn };
--- homework-10/src/scripts.ts
+++ homework-10/src/scripts.ts
@@ -1,4 +1,24 @@
-import { box } from "./assets/ts/const";
+import { addTitle, addText, addURL, addBtn, box } from "./assets/ts/const";
 import { reload } from "./assets/ts/helper-func";
+import { PlayerCard } from "./assets/ts/interfaces";
+import { post, put } from "./assets/ts/restAPI";
 
 if (box.childElementCount === 0) reload();
+
+addBtn.addEventListener("click", () => {
+  if (addTitle.value === "" || addText.value === "" || addURL.value === "") {
+    alert(`Please input all values!`);
+  } else {
+    post({
+      skin: addURL.value,
+      title: addTitle.value,
+      text: addText.value,
+    }).then((res) => {
+      reload();
+      alert("Added successfully!");
+      addURL.value = "";
+      addTitle.value = "";
+      addText.value = "";
+    });
+  }
+});
