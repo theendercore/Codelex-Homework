13f4269b4d265cfa124fe8c03f7c2007770ef730
theendercore <theendercore@gmail.com>
2023-01-26 22:09:47 +0200

    .init tailwind

--- homework-12/src/App.tsx
+++ homework-12/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card bg-slate-200">
+      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
