5a60183f3d6dae2796b9920914387bffac7373ba
theendercore <theendercore@gmail.com>
2023-01-27 15:14:43 +0200

    aaa

--- homework-12/src/App.tsx
+++ homework-12/src/App.tsx
@@ -1,32 +1,59 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+
+interface item { id: number, todo: JSX.Element }
+
+
+
+const ListElement: React.FC = (remove:any) => {
+  const [checked, setChecked] = useState<boolean>(false)
+  return (
+    <div className='ListElement my-7'>
+      <input
+        type="checkbox"
+        className="m-3 h-10 w-10 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+        onClick={() => setChecked(!checked)} />
+      <input
+        type="text"
+        className={(checked ? 'line-through' : '') + " bg-slate-900 border-b-2 border-slate-300"} />
+      <button className="" onClick={ remove }>x</button>
+    </div>
+  )
+}
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState<item[]>([])
+
+  const removeTodo = (todoToRemove: item) => {
+    let updatedTodo: item[] = list.filter(todo => todo.todo.key != todoToRemove.todo.key);
+    setList(updatedTodo);
+  }
+
+  const addTodo = () => {
+    const newTodo = {
+      id: list.length + 1,
+      todo: <ListElement />
+    }
+
+    setList([...list, newTodo])
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="App bg-slate-900">
+      <h1 className='text-5xl text-center'>To do List:</h1>
+      <div className="to-do flex flex-col content-center">
+        <ListElement />
+        {list.map((todo) => {
+          return <ListElement key={todo.id}/>
+        })}
+        <button className="bg-slate-700 rounded-full text-5xl" onClick={addTodo}>
+          +
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
   )
 }
