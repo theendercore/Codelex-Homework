bf985a210693a337b3d0bb3d196f8636ce711a6f
theendercore <theendercore@gmail.com>
2023-02-03 18:21:36 +0200

    init hw-14

---
+++ homework-14/1/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App container mx-auto my-30">
+     <h1 className="text-5xl">Hello</h1> 
+    </div>
+  )
+}
+
+export default App
