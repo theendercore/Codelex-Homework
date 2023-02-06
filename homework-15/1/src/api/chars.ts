bd596ca548bd11fad2db05ec59097914332c4d39
theendercore <theendercore@gmail.com>
2023-02-06 18:51:45 +0200

    aesthetic changes

--- homework-15/1/src/api/chars.ts
+++ homework-15/1/src/api/chars.ts
@@ -1,17 +1,18 @@
 import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
 const DB_URL = "https://rickandmortyapi.com/api/character";
-const db_page = "https://rickandmortyapi.com/api/character/?page="
+const db_page = "https://rickandmortyapi.com/api/character/?page=";
 
-export async function getChars() {
-  return await axios.get<CharList>(DB_URL).then(async ({ data }) => data);
+export function getChars() {
+  return axios.get<CharList>(DB_URL).then(({ data }) => data);
 }
 
 export function getCharsParams({
   pageParam = 0,
 }: QueryFunctionContext<string[], any>) {
-  console.log(pageParam);
-  return axios.get<CharList>(`${db_page}${pageParam}`).then(async ({ data }) => data);
+  return axios
+    .get<CharList>(`${db_page}${pageParam}`)
+    .then(async ({ data }) => data);
 }
 
 export function getCharByID(id: string) {
