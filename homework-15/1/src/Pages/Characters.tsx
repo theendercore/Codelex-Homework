217d239bcf71749f4cc232376586d19e33cc6a3d
theendercore <theendercore@gmail.com>
2023-02-06 18:33:56 +0200

    finished load more

--- homework-15/1/src/Pages/Characters.tsx
+++ homework-15/1/src/Pages/Characters.tsx
@@ -17,7 +17,7 @@ export default function Characters() {
   } = useInfiniteQuery({
     queryKey: ["chars"],
     queryFn: getCharsParams,
-    getNextPageParam: (lastPage) => lastPage.info.next?.split("=")[-1],
+    getNextPageParam: (lastPage) => lastPage.info.next?.split("=").slice(-1)[0],
     // keepPreviousData: true,
   });
 
@@ -27,7 +27,7 @@ export default function Characters() {
     return <Container input={<h1>Error {JSON.stringify(error)}</h1>} />;
 
   return (
-    <div className="container mx-auto my-5">
+    <div className="container mx-auto my-5 flex flex-col items-center">
       <h2 className="my-8 text-center text-4xl font-extrabold">Characters</h2>
       <div className="grid grid-cols-4 gap-10">
         {data.pages.map(({ results }, i) => (
@@ -41,6 +41,7 @@ export default function Characters() {
       <button
         onClick={() => fetchNextPage()}
         disabled={!hasNextPage || isFetchingNextPage}
+        className="m-10 rounded-xl bg-slate-700 px-10 py-4 shadow-xl hover:bg-slate-900 hover:text-slate-100"
       >
         {isFetchingNextPage
           ? "Loading more..."
