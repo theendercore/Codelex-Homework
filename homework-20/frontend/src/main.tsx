b5eb73a6294500cb0f783c7d9214125fd8aace59
theendercore <theendercore@gmail.com>
2023-02-22 15:10:13 +0200

    fixed my install mistakes

--- homework-20/frontend/src/main.tsx
+++ homework-20/frontend/src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Router } from '@tanstack/react-router';
+import { RouterProvider } from '@tanstack/react-router';
+
+const routeTree = rootRoute.addChildren([indexRoute])
+
+const router = new Router({ routeTree })
+
+declare module '@tanstack/router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
