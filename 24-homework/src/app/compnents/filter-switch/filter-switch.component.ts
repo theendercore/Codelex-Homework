46cfc4ddc972320e84b65785a4b32487cafc13c2
theendercore <theendercore@gmail.com>
2023-03-05 18:29:26 +0200

    filter-switch completed

--- 24-homework/src/app/compnents/filter-switch/filter-switch.component.ts
+++ 24-homework/src/app/compnents/filter-switch/filter-switch.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { AnimalType, AnimalTypeList } from '../../models/Animal';
 
 @Component({
   selector: 'app-filter-switch',
-  templateUrl: './filter-switch.component.html'
+  templateUrl: './filter-switch.component.html',
 })
 export class FilterSwitchComponent {
+  private animalTypes: Array<AnimalType | 'All'> = ["All", ...AnimalTypeList];
+  state: AnimalType | 'All' = 'All';
 
+  onClick() {
+    this.animalTypes.push(this.animalTypes.shift() || 'All');
+    this.state = this.animalTypes[0];
+  }
 }
