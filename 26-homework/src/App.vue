541f4bb9b578318c317e6938cbe9ada3196b44f4
theendercore <theendercore@gmail.com>
2023-03-07 13:55:04 +0200

    hw-26 init

---
+++ 26-homework/src/App.vue
@@ -0,0 +1,14 @@
+
+<script setup lang="ts">
+// import ButtonC from "./components/ButtonC.vue";
+import ButtonC from "@/components/ButtonC.vue";
+</script>
+
+
+<template>
+ <div class="container m-auto p-10">
+<h1 class="text-xl">Hello</h1>
+
+
+ </div> 
+</template>
---
+++ 26-homework/src/components/ButtonC.vue
@@ -0,0 +1,3 @@
+<template>
+    <button class="bg-slate-700 rounded-xl">Hi</button>
+</template>
